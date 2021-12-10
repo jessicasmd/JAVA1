3696697634f9d5c0404907cbb91fc9f727d6c2f9
Jéssica <jessicaasmd@gmail.com>
2021-12-10 20:09:51 -0300

    10_12

--- Atividade_Java3/Questao2DoWhile.java
+++ Atividade_Java3/Questao2DoWhile.java
@@ -3,125 +3,34 @@ package Atividade_Java3;
 import java.util.Scanner;
 
 public class Questao2DoWhile {
-// Uma empresa desenvolveu uma pesquisa para saber as características psicológicas dos 
-//	indivíduos de uma região. Para tanto, a cada uma das pessoas era perguntado: idade, 
-//	sexo (1-feminino / 2-masculino / 3-Outros), e as opções (1, se a pessoa era calma; 2, 
-//	se a pessoa era nervosa e 3, se a pessoa era agressiva). Pede-se para elaborar um sistema 
-//	que permita ler os dados de 150 pessoas, calcule e mostre: (WHILE)
-//	o número de pessoas calmas; 
-//	o número de mulheres nervosas; 
-//	o número de homens agressivos; 
-//	o número de outros calmos;
-//	o número de pessoas nervosas com mais de 40 anos; 
-//	o número de pessoas calmas com menos de 18 anos.
+// 6 -Escrever um programa que receba vários números inteiros no teclado. 
+//	E no final imprimir a média dos números múltiplos de 3. Para sair digitar 
+//	0(zero).(DO...WHILE)
 
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner ler = new Scanner (System.in);
+		Scanner leia = new Scanner (System.in);
 		
-		int sexo, idade, temp;
-		int pesCal=0,mulNer=0,homAgr=0,outCal=0,pesNerMa40=0,pesCalMe18=0, pessoas=0;
-
-		while(pessoas<150)
+		int num,cont=0,somaNum=0;
+		float media;
+		
+		System.out.println("\nInforme um número ");
+		num=leia.nextInt();
+		
+		do
 		{
-			
-			System.out.println("\nInforme sua idade: ");
-			idade=ler.nextInt();
-			System.out.println("\nInforme seu sexo");
-			System.out.println("\n1- Feminino");
-			System.out.println("\n2- Masculino");
-			System.out.println("\n3- Outros");
-			sexo=ler.nextInt();
-			
-			switch(sexo)
+			if(num%3==0) 
 			{
-				case 1:
-					System.out.println("\nVocê se considera uma pessoa: ");
-					System.out.println("\n1- Calma.");
-					System.out.println("\n2- Nervosa.");
-					System.out.println("\n3- Agressiva.");
-					temp=ler.nextInt();
-					if(idade<18 && temp==1)
-					{
-						pesCalMe18++;
-						pesCal++;
-					}
-					else if(idade>40 && temp==2)
-					{
-						pesNerMa40++;
-						mulNer++;
-					}
-					else if(temp==2)
-					{
-						mulNer++;
-					}
-					else if(temp==1)
-					{
-						pesCal++;
-					}
-					break;
-										
-					
-				case 2:
-					System.out.println("\nVocê se identifica como uma pessoa: ");
-					System.out.println("\n1- Você é calmo.");
-					System.out.println("\n2- Você é nervoso.");
-					System.out.println("\n3- Você é agressivo.");
-					temp=ler.nextInt();
-					if(idade<18 && temp==1)
-					{
-						pesCalMe18++;
-						pesCal++;
-					}
-					else if(temp==3)
-					{
-						homAgr++;
-					}
-					else if(idade>40 && temp==2)
-					{
-						pesNerMa40++;
-					}
-					else if(temp==1)
-					{
-						pesCal++;
-					}
-					break;
-					
-					
-				case 3:
-					System.out.println("\nVocê se identifica como uma pessoa: ");
-					System.out.println("\n1- Você é calmo.");
-					System.out.println("\n2- Você é nervoso.");
-					System.out.println("\n3- Você é agressivo.");
-					temp=ler.nextInt();
-					if(idade<18 && temp==1)
-					{
-						pesCalMe18++;
-						outCal++;
-						pesCal++;
-					}
-					else if(idade>40 && temp==2)
-					{
-						pesNerMa40++;
-					}
-					else if(temp==1)
-					{
-						outCal++;
-						pesCal++;
-					}
-					break;
-					default:
-					System.out.println("\nOpção inválida!");			
+			cont++;
+			somaNum+=num;
 			}
-			pessoas++;
+			System.out.println("\nInforme um número ");
+			num=leia.nextInt();
 			
-		}
-		System.out.println("\nO número de pessoas calmas é de "+pesCal);
-		System.out.println("\nO número de mulheres nervosas é de "+mulNer);
-		System.out.println("\nO número de homens agressivos é de "+homAgr);
-		System.out.println("\nO número de outros calmos é de "+outCal);
-		System.out.println("\nO número de pessoas nervosas com mais de 40 anos é de "+pesNerMa40);
-		System.out.println("\nO número de pessoas calmas com menos de 18 anos é de "+pesCalMe18);
+		}while(num!=0);
+		
+		media=(float)somaNum/cont;
+		System.out.printf("\nForam informados %d número(s) múltiplo(s) de 3, sua soma é de %d e sua média é %.2f ",cont,somaNum,media);
 	}
 }
--- Atividade_Java3/Questao2While.java
+++ Atividade_Java3/Questao2While.java
@@ -3,30 +3,125 @@ package Atividade_Java3;
 import java.util.Scanner;
 
 public class Questao2While {
+	// 4 -Uma empresa desenvolveu uma pesquisa para saber as características psicológicas dos 
+//		indivíduos de uma região. Para tanto, a cada uma das pessoas era perguntado: idade, 
+//		sexo (1-feminino / 2-masculino / 3-Outros), e as opções (1, se a pessoa era calma; 2, 
+//		se a pessoa era nervosa e 3, se a pessoa era agressiva). Pede-se para elaborar um sistema 
+//		que permita ler os dados de 150 pessoas, calcule e mostre: (WHILE)
+//		o número de pessoas calmas; 
+//		o número de mulheres nervosas; 
+//		o número de homens agressivos; 
+//		o número de outros calmos;
+//		o número de pessoas nervosas com mais de 40 anos; 
+//		o número de pessoas calmas com menos de 18 anos.
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-			Scanner leia = new Scanner(System.in);
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			Scanner ler = new Scanner (System.in);
 			
-			int numVariados, somaMedia=0, entradaMedia=0;
-			float media;
-			
-			do
+			int sexo, idade, temp;
+			int pesCal=0,mulNer=0,homAgr=0,outCal=0,pesNerMa40=0,pesCalMe18=0, pessoas=0;
+
+			while(pessoas<150)
 			{
-				if(numVariados%3==0)
-				{
-					somaMedia+=numVariados;
-					entradaMedia++;
-				}
 				
-				System.out.println("\n Entre com o valor: ");
-				numVariados=leia.nextInt();
+				System.out.println("\nInforme sua idade: ");
+				idade=ler.nextInt();
+				System.out.println("\nInforme seu sexo");
+				System.out.println("\n1- Feminino");
+				System.out.println("\n2- Masculino");
+				System.out.println("\n3- Outros");
+				sexo=ler.nextInt();
+				
+				switch(sexo)
+				{
+					case 1:
+						System.out.println("\nVocê se considera uma pessoa: ");
+						System.out.println("\n1- Calma.");
+						System.out.println("\n2- Nervosa.");
+						System.out.println("\n3- Agressiva.");
+						temp=ler.nextInt();
+						if(idade<18 && temp==1)
+						{
+							pesCalMe18++;
+							pesCal++;
+						}
+						else if(idade>40 && temp==2)
+						{
+							pesNerMa40++;
+							mulNer++;
+						}
+						else if(temp==2)
+						{
+							mulNer++;
+						}
+						else if(temp==1)
+						{
+							pesCal++;
+						}
+						break;
+											
+						
+					case 2:
+						System.out.println("\nVocê se identifica como uma pessoa: ");
+						System.out.println("\n1- Você é calmo.");
+						System.out.println("\n2- Você é nervoso.");
+						System.out.println("\n3- Você é agressivo.");
+						temp=ler.nextInt();
+						if(idade<18 && temp==1)
+						{
+							pesCalMe18++;
+							pesCal++;
+						}
+						else if(temp==3)
+						{
+							homAgr++;
+						}
+						else if(idade>40 && temp==2)
+						{
+							pesNerMa40++;
+						}
+						else if(temp==1)
+						{
+							pesCal++;
+						}
+						break;
+						
+						
+					case 3:
+						System.out.println("\nVocê se identifica como uma pessoa: ");
+						System.out.println("\n1- Você é calmo.");
+						System.out.println("\n2- Você é nervoso.");
+						System.out.println("\n3- Você é agressivo.");
+						temp=ler.nextInt();
+						if(idade<18 && temp==1)
+						{
+							pesCalMe18++;
+							outCal++;
+							pesCal++;
+						}
+						else if(idade>40 && temp==2)
+						{
+							pesNerMa40++;
+						}
+						else if(temp==1)
+						{
+							outCal++;
+							pesCal++;
+						}
+						break;
+						default:
+						System.out.println("\nOpção inválida!");			
 				}
-			while(numVariados!= 0);
-			
-			media=(float)somaMedia/entradaMedia;
-			System.out.println("\nA média dos números múltiplos de 3 é: "+media);
+				pessoas++;
+				
+			}
+			System.out.println("\nO número de pessoas calmas é de "+pesCal);
+			System.out.println("\nO número de mulheres nervosas é de "+mulNer);
+			System.out.println("\nO número de homens agressivos é de "+homAgr);
+			System.out.println("\nO número de outros calmos é de "+outCal);
+			System.out.println("\nO número de pessoas nervosas com mais de 40 anos é de "+pesNerMa40);
+			System.out.println("\nO número de pessoas calmas com menos de 18 anos é de "+pesCalMe18);
+		}
 	}
-
-}
