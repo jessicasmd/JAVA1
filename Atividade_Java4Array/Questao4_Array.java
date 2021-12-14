861b5a9e6dfb322f87baf14b7332e477238adfef
Jéssica <jessicaasmd@gmail.com>
2021-12-14 09:51:06 -0300

    13_12

--- Atividade_Java4Array/Questao4_Array.java
+++ Atividade_Java4Array/Questao4_Array.java
@@ -15,23 +15,87 @@ public class Questao4_Array {
 		// TODO Auto-generated method stub
 		Scanner leia = new Scanner(System.in);
 		
-		int op;
+		int constante,linha, coluna,op;
 		float [][] matriz1= new float [2][2];
 		float [][] matriz2= new float [2][2];
-		float linha, coluna;
+		float [][] matriz3= new float [2][2];
+	
+		for (linha=0; linha<2;linha++)
+		{
+			for(coluna=0; coluna<2; coluna++)
+			{
+				System.out.println("Informe o primeiro número: ");
+				matriz1[linha][coluna] = leia.nextFloat();
+				System.out.println("Informe o segundo número: ");
+				matriz2[linha][coluna] = leia.nextFloat();
+			}
+		}
+		do
+		{
+			System.out.println("\nEscolha a opção: ");
+			System.out.println("1 - Somar as duas matrizes.");
+			System.out.println("2 - Subtrair a primeira matriz da segunda.");
+			System.out.println("3 - Adicionar uma constante as duas matrizes.");
+			System.out.println("4 - Imprimir as matrizes.");
+			System.out.println("0 - Sair do sistema");
+			op=leia.nextInt();
+			
+			switch(op)
+			{
+			case 1:
+				for (linha=0;linha<2;linha++)
+				{
+					for(coluna=0;coluna<2;coluna++)
+					{
+						matriz3[linha][coluna]= matriz1[linha][coluna] + matriz2[linha][coluna];
+								System.out.println("\nSoma das matrizes: "+matriz3[linha][coluna]);
+					}
+				}
+				break;
+			case 2:
+				for (linha=0;linha<2;linha++)
+				{
+					for(coluna=0;coluna<2;coluna++)
+					{
+						matriz3[linha][coluna]= matriz2[linha][coluna] - matriz1[linha][coluna];
+								System.out.println("\nA diferença das matrizes: "+matriz3[linha][coluna]);
+					}
+				}
+				break;
+			case 3:
+				System.out.println("Digite o valor da constante: ");
+				constante=leia.nextInt();
+				for (linha=0;linha<2;linha++)
+				{
+					for(coluna=0;coluna<2;coluna++)
+					{
+						matriz1[linha][coluna]+= constante;
+						matriz2[linha][coluna]+= constante;
+						
+								System.out.println("1ª Matriz: "+matriz1[linha][coluna]);
+								System.out.println("2ª Matriz: "+matriz2[linha][coluna]);
+					}
+				}
+				break;
+			
+			case 4: 
+				
+				for (linha=0;linha<2;linha++)
+				{
+					for(coluna=0;coluna<2;coluna++)
+					{
+											
+								System.out.println("1ª Matriz: "+matriz1[linha][coluna]);
+								System.out.println("2ª Matriz: "+matriz2[linha][coluna]);
+					}
+				}
+				break;
+				default:
+					System.out.println("OPÇÃO INVÁLIDA OU SAIR DO PROGRAMA");
+			}
+		}	while(op!=0);
 		
-		System.out.println("\nInforme o dado: ");
-		System.out.println("1 - Somar as duas matrizes.");
-		System.out.println("2 - Subtrair a primeira matriz da segunda.");
-		System.out.println("3 - Adicionar uma constante as duas matrizes.");
-		System.out.println("4 - Imprimir as matrizes.");
-		op=leia.nextInt();
-		
-		System.out.println("Informe o primeiro número: ");
-		matriz1 = [linha][coluna]leia.nextFloat();
-		System.out.println("Informe o segundo número: ");
-		matriz2 = [linha][coluna]leia.nextFloat();
-		
+				
 	}
 
 }
