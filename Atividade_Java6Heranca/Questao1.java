dd96f58e2273591f5b74b9fa4f27e7763c579a62
Jéssica <jessicaasmd@gmail.com>
2021-12-15 16:02:38 -0300

    15_12

--- Atividade_Java6Heranca/Questao1.java
+++ Atividade_Java6Heranca/Questao1.java
@@ -1,19 +1,19 @@
-package Atividade_Java6Heranca;
+package  Atividade_Java6Heranca ;
 
-public class Questao1 {
-// 1- Crie uma hierarquia de classes conforme abaixo com os seguintes atributos e 
-//	comportamentos (observe a tabela), utilize os seus conhecimentos e distribua as 
-//	características de forma que tudo o que for comum a todos os animais fique na classe Animal: 
+public  class  Questao1 {
+// 1- Crie uma hierarquia de classes conforme abaixo com os seguintes atributos e
+// 	comportamento (observe a tabela), utilize os seus conhecimentos e distribuição como
+// 	características de forma que tudo o que para comum a todos os animais fique na classe Animal:
 	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public  static  void  main ( String [] args ) {
+		// TODO stub do método gerado automaticamente
 		
-		Questao1base cachorro = new Questao1base("Amora", "1","Au au au", 0, 20);
-		cachorro.imprimirInfo();
-		Questao1base cavalo = new Questao1base("Coragem", "5", "Riririri", 0, 30);
-		cavalo.imprimirInfo();
-		Questao1base preguica = new Questao1base("Lebre", "10", "Zizizizizi", 100,5);
-		preguica.imprimirInfo();
+		Questao1base cachorro =  new  Questao1base ( " Amora " , " 1 " , " Au au au " , 0 , 20 );
+		cachorro . imprimirInfo ();
+		Questao1base cavalo =  new  Questao1base ( " Coragem " , " 5 " , " Riririri " , 0 , 30 );
+		cavalo . imprimirInfo ();
+		Questao1base preguica =  new Questao1base ( " Lebre " , " 10 " , " Zizizizizi " , 100 , 5 );
+		preguica.imprimirInfo ();
 		
 	}
 
--- Atividade_Java6Heranca/Questao1base.java
+++ Atividade_Java6Heranca/Questao1base.java
@@ -9,7 +9,7 @@ public class Questao1base {
 		private int correr;
 			
 		
-		public Questao1base (String nome, String idade, String som,int subir, int correr)
+		public Questao1base (String nome, String idade, String som, int subir, int correr)
 		
 		{
 			this.nome=nome;
@@ -19,61 +19,51 @@ public class Questao1base {
 			this.correr=correr;		
 			
 		}
+		
+		public void imprimirInfo()
+		{
+			System.out.println("\n Nome:  "+nome+"\nIdade  "+idade+"\nSom: "+som+"\nSubiu quantas árvores: "+subir+, "\nCorreu quanto km: "+correr);
 
-
+			
 		public String getNome() {
 			return nome;
 		}
-
 
 		public void setNome(String nome) {
 			this.nome = nome;
 		}
 
-
 		public String getIdade() {
 			return idade;
 		}
-
 
 		public void setIdade(String idade) {
 			this.idade = idade;
 		}
 
-
 		public String getSom() {
 			return som;
 		}
-
 
 		public void setSom(String som) {
 			this.som = som;
 		}
 
-
 		public int getSubir() {
 			return subir;
 		}
-
 
 		public void setSubir(int subir) {
 			this.subir = subir;
 		}
 
-
 		public int getCorrer() {
 			return correr;
 		}
 
-
 		public void setCorrer(int correr) {
 			this.correr = correr;
 		}
-
-
-		public void imprimirInfo() {
-			// TODO Auto-generated method stub
-			
-		}
 		
+	}
 }
