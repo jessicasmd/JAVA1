1402d20d669b00572d71da4cefb0e7b5a57f50ef
Jéssica <jessicaasmd@gmail.com>
2021-12-16 22:31:28 -0300

    16_12

--- Atividade_Java6Heranca/Questao2.java
+++ Atividade_Java6Heranca/Questao2.java
@@ -6,7 +6,21 @@ public class Questao2 {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Cachorro1 cachorro = new Cachorro1 ("Amora", "1", true);
+		cachorro.imprimirInfo();
+		cachorro.imprimirMovimento();
+				
+		Cavalo1 cavalo =  new  Cavalo1 ( "Coragem", "5", true);
+		cavalo.imprimirInfo();
+		cavalo.imprimirMovimento();
+			
+		Preguica1 preguica =  new Preguica1( "Lebre ", "10 ", true);
+		preguica.imprimirInfo();
+		preguica.imprimirMovimento();
+		
 	}
 
+		
 }
+
+
